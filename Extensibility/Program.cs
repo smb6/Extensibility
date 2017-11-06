b4ad00d1dec9243d1570fedc2060e2fd858ef877
Pablo Cusnir <pabloc@kayhut.com>
2017-11-06 18:20:37 +0200

    Add project files.

---
+++ Extensibility/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigrator = new DbMigrator(new ConsoleLogger());
+            dbMigrator.Migrate();
+
+            var dbMigratorLogFile = new DbMigrator(new FileLogger(@"E:\Projects\CSharpFundamentals\LOG.txt"));
+            dbMigratorLogFile.Migrate();
+        }
+    }
+}
